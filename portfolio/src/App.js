717ae325b795960a803e892f7e66efdc6492188a
Parth Bakhda <Bakhdaparth@gmail.comgit config --global user.email Bakhdaparth@gmail.comgit config --global user.email Parth>
2023-06-15 22:12:17 -0700

    Started app.js and added images folder

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [selectedSection, setSelectedSection] = useState('About Me');
 
-export default App;
+  const handleNavigationClick = (section) => {
+    setSelectedSection(section);
+  };
+
+  const renderSection = () => {
+    switch (selectedSection) {
+      case 'About Me':
+        return (
+          <div>
+            <h2>About Me</h2>
+            {/* "C:\Users\bakhd\Desktop\bootcamp\Portfolio-for-React\Images\IMG_0195.jpeg" Hello, my name is Parth Bakhda. I was born in India, raised in Arizona. Some of my interest include skiing, hiking, going to the gym, and binge watching tv shows. As a young child I had two full-time working-class parents, so I had to learn everything on my own, that did not stop me for achieving. In 5th grade I was in a commercial for the Diary Counsil for my outstanding attendance and grades. I like learning new skills thus, my reason for joining this coding bootcamp. I find it invigorating to apply what I have learned into creating beautiful works of art thru coding. My goal is to succeed in this course and after find a career where I can grow internally and become financially stable. */}
+          </div>
+          );
